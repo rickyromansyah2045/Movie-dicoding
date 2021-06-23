281301e34cb33fcf6bce4f4a6249ba500a4d2ce0
rickyromansyah2045 <rickyromansyah54@gmail.com>
2021-06-23 22:30:23 +0700

    Update

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: 'a2ccf71e9dbdfc08496f5c629bec315e',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
